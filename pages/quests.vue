b38cde10b28132c4f6c263790db64cc245b299ed
Iho Somnam <ihosomnam4741@gmail.com>
2023-09-09 00:31:10 +0700

    Update quests.vue

--- pages/quests.vue
+++ pages/quests.vue
@@ -2,10 +2,10 @@
   <div>
     <BlockColumn>
       <BlockHeader title="Your Profile">
-        <code v-if="pending">Loading...</code>
+        <code>Loading...</code>
         <div class="flex gap-4 justify-center items-center p-4 cursor-pointer shadow-md bg-white rounded">
-          <img src="`https://www.bungie.net${data.profilePicturePath}`" alt="D2 Profile" class="rounded-full w-12 h-12 outline outline-green-500">
-          <h2 class="font-bold text-2xl">{{ data.uniqueName }}</h2>
+          <img src="/ganyu.png" alt="D2 Profile" class="rounded-full w-12 h-12 outline outline-green-500">
+          <h2 class="font-bold text-2xl">C A T S H O O T ! ! !</h2>
         </div>
 
       </BlockHeader>
@@ -19,8 +19,5 @@
 
 <script setup>
 
-const route = useRoute()
-const code = useState('code', () => route.query.code)
-const { data, pending } = await useFetch('/api/d2/profile')
 
 </script>
